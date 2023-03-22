a14de25530324af72ef5f0212c78160fc9b97382
aravgithubb <128202013+aravgithubb@users.noreply.github.com>
2023-03-22 14:51:32 +0530

    Update MusicData.js

--- Tests/Music Festival Test/MusicData.js
+++ Tests/Music Festival Test/MusicData.js
@@ -1,14 +1,19 @@
+//IMPORTING THE SELENIUM WEBDRIVER AND CHROME
 const {By,Key,Builder} = require("selenium-webdriver");
 require("chromedriver");
  
 async function festival()
 {
- 
+ 	// LAUNCHING THE BROWSER
        let driver = await new Builder().forBrowser("chrome").build();
+	// LAUNCH THE URL
        await driver.get("http://localhost:4200/s");
+	//FIND ELEMENTS
 	  let elements= await driver.findelements(By.xpath("/html/body/app-root/app-festivals/ol/li"));
+	// EXTRACTING THE ELEMENTS AND PRINTING THE TEXT ON THE CONSOLE and validating the element is displayed
 	  for(let elem of elements){
-		console.log(await elem.getText());
+		Assert.assertTrue(await elem.isDisplayed());
+		console.log("Element " + elem.getText());
 	  }
 
 }    
