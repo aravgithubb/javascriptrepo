ec6c5a023836bb90b0c3166f85fefed1036f3f68
aravgithubb <128202013+aravgithubb@users.noreply.github.com>
2023-03-22 15:04:29 +0530

    Update MusicVerify.js

--- Tests/Music Festival Test/MusicVerify.js
+++ Tests/Music Festival Test/MusicVerify.js
@@ -1,13 +1,20 @@
+//IMPORTING THE SELENIUM WEBDRIVER AND CHROME
+
 const {By,Key,Builder} = require("selenium-webdriver");
 require("chromedriver");
  
 async function festival()
 {
  
+ 	     // LAUNCHING THE BROWSER 
        let driver = await new Builder().forBrowser("chrome").build();
+      	// LAUNCH THE URL
        await driver.get("http://localhost:4200/s");
+       // declaring the expected string.
        var expectedtitle="EaCodingTests";
+       // getting the actual string
        var actualtitle=driver.getTitle();
+       // verifying the actual and expected titles
        Assert.assertEquals(actualtitle, expectedtitle);
 
 }    
