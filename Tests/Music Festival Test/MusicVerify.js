0e5370d19df785e79e3c851d3862e2c75ba83eb8
aravgithubb <128202013+aravgithubb@users.noreply.github.com>
2023-03-23 07:03:09 +0530

    Update MusicVerify.js

--- Tests/Music Festival Test/MusicVerify.js
+++ Tests/Music Festival Test/MusicVerify.js
@@ -10,12 +10,18 @@ async function verifyTitle()
        let driver = await new Builder().forBrowser("chrome").build();
       	// LAUNCH THE URL
        await driver.get("http://localhost:4200/s");
+       // implicit wati to sync with webdriver.
+       await driver.manage().setTimeouts( {implicit : 10000} );
+       // maximize the browser
+       await driver.manage().window().fullscreen();
        // declaring the expected string.
        var expectedtitle="EaCodingTests";
        // getting the actual string
        var actualtitle=driver.getTitle();
-       // verifying the actual and expected titles
-       Assert.assertEquals(actualtitle, expectedtitle);
+       // validating the actual and expected text 
+      assert.strictEqual(actualtitle,expectedtitle);
+      // quit the browser
+      await driver.quit();
 
 }    
 verifyTitle()
